1b8fedf0b5ec30577050020f9c3f3fe2864c100a
NssWay <115477023+NssWay@users.noreply.github.com>
2023-10-27 13:59:00 +0300

    Add files via upload

---
+++ 13.pas
@@ -0,0 +1,17 @@
+﻿program z13;
+var a,b,n,sr:real;
+begin
+  a:=3; 
+  b:=6;
+  n:=1;
+  begin
+  repeat
+    a:=a+b;
+    n:=n+1;
+  until n >= 10;
+  writeln('Сумма ',a);
+  writeln('Количество ',n);
+  end;
+    sr:=a/n;
+  writeln('Среднее арифмитическое ',sr);
+end.
---
+++ 4.pas
@@ -0,0 +1,9 @@
+﻿program z4;
+var
+  i, sum: integer;
+begin
+  sum := 0;
+  for i := 4 to 37 do
+    sum := sum + i * i;
+  writeln('Сумма квадратов натуральных чисел от 4 до 37: ', sum);
+end.
